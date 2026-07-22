d958e50151a18ca8c1724bd1e6e03aff7a09bf41
RenzzoPalomino <renzzoomar@gmail.com>
2026-07-22 13:26:55 -0500

    Limpieza de codigo no utilizado

--- SuiteCoreBackend/Services/Interfaces/IAuthService.cs
+++ SuiteCoreBackend/Services/Interfaces/IAuthService.cs
@@ -13,12 +13,6 @@ namespace SuiteCoreBackend.Services.Interfaces
         /// <param name="request">Credenciales de acceso.</param>
         /// <returns>Respuesta de autenticación o null si no se pudo autenticar.</returns>
         Task<LoginResponseDto?> LoginAsync(LoginRequestDto request, string clientIp);
-        /// <summary>
-        /// No se utiliza actualmente, pero podría ser útil para futuras implementaciones de cierre de sesión.
-        /// </summary>
-        /// <param name="sessionId"></param>
-        /// <param name="username"></param>
-        /// <returns></returns>
-        Task LogoutAsync(string sessionId, string username);
+        
     }
 }
